49179eac3dc7e492713280015460175afc19e982
faculuceroh <facundoluceroh@gmail.com>
2025-11-17 16:24:45 -0300

    Add files via upload

---
+++ CEjecutora.cs
@@ -0,0 +1,56 @@
+﻿//Facundo Lucero 30794
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial_GrupoB
+{
+    public class CEjecutora
+    {
+        public static void Main()
+        { 
+            Console.WriteLine("INGRESE EL PORCENTAJE DE DESCUENTO POR PAGO ANUAL");
+            float descuento = float.Parse(Console.ReadLine());
+            Cplan.Descuentoanual = descuento;
+
+            List<Cplan> planes = new List<Cplan>();
+            Console.WriteLine("INGRESE EL TITULO DEL PLAN A BUSCAR");
+            string titulo = Console.ReadLine().ToUpper();
+            while (titulo != "FIN")
+            {
+                Console.WriteLine("INGRESE EL CODIGO DEL PLAN");
+                string codigo = Console.ReadLine();
+                Console.WriteLine("INGRESE EL COSTO MENSUAL DEL PLAN");
+                float costomensual = float.Parse(Console.ReadLine());
+                Cplan plan = new Cplan(codigo, titulo);
+                plan.SetCostoMensual (costomensual);
+                planes.Add(plan);
+                Console.WriteLine("INGRESE OTRO TITULO DE PLAN O FIN PARA FINALIZAR");
+                titulo = Console.ReadLine().ToUpper();
+            }
+            if (planes.Count > 0)
+            {
+                Cplan PlanMasEconomico = planes[0];
+                foreach(Cplan plan in planes)
+                {
+                    if(plan.MasEconomico(PlanMasEconomico))
+                    {
+                        PlanMasEconomico = plan;
+                    }
+                }
+                Console.WriteLine($"Los datos del plan mas economico son:{PlanMasEconomico.DarDatos()}");
+                Console.WriteLine($"El costo anual del plan mas economico es :{PlanMasEconomico.CalcularCosto()}");
+                Console.WriteLine($"El costo anual con descuento del plan mas economico es :{PlanMasEconomico.CalcularCosto(true)}");
+                Console.WriteLine($"El total recaudado por todos los planes es:{ Cplan.TotalRecaudado(planes)}");
+
+
+            }
+            else
+            {
+                Console.WriteLine("No se ingreso ningun plan");
+            }
+        }
+    }
+}
---
+++ CPlan.cs
@@ -0,0 +1,94 @@
+﻿//Facundo Lucero 30794
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial_GrupoB
+{
+    public class Cplan
+    {
+        private string codigo;
+        private string titulo;
+        private float costomensual;
+        private static float descuentoanual;
+
+        public void SetCostoMensual(float costo)
+        {
+            this.costomensual = costo;
+        }
+
+        public float GetCostoMensual()
+        {
+            return costomensual;
+        }
+
+        public static float Descuentoanual
+        {
+            get { return descuentoanual; }
+            set { descuentoanual = value; }
+        }
+
+        public Cplan(string Codigo, string Titulo)
+        {
+            this.codigo = Codigo;
+            this.titulo = Titulo;
+        }
+
+
+        public float CalcularCosto()
+        {
+            float Costoanual = this.costomensual * 12;
+            return Costoanual;
+        }
+
+        public float CalcularCosto(bool anual)
+        {
+            float costoanual = CalcularCosto();
+            if (anual)
+            {
+                float descuento = (costoanual * Descuentoanual) /  100;
+                return (costoanual) - descuento;
+            }
+            else
+            {
+                return costoanual;
+            }
+        }
+
+        public string DarDatos()
+        {
+            string datos = $"Codigo :  {this.codigo} || Titulo: {this.titulo}|| Coste Mensual: {this.costomensual}";
+            return datos;
+        }
+
+        public string DarDatos(bool anual)
+        {
+            if (anual)
+            {
+                string datosanual = $"Codigo : {this.codigo} || Titulo: {this.titulo}|| Coste Mensual: {this.costomensual} || Costo Anual con descuento: {CalcularCosto(true)}";
+                return datosanual;
+            }
+            else
+            {
+                return DarDatos();
+            }
+        }
+
+        public bool MasEconomico(Cplan PlanBarato)
+        {
+            return this.CalcularCosto() < PlanBarato.CalcularCosto();
+        }
+
+        public static float TotalRecaudado(List<Cplan> planes)
+        {
+            float total = 0;
+            foreach (Cplan plan in planes)
+            {
+                total += plan.CalcularCosto();
+            }
+            return total;
+        }
+    }
+}
